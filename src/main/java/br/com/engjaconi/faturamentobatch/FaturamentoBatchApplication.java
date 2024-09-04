3e45677074f31653435d85828c807e7c7ee6af14
Tiago Jaconi <engjaconi@gmail.com>
2024-09-04 18:25:57 -0300

    Spring Batch Inicial

---
+++ src/main/java/br/com/engjaconi/faturamentobatch/FaturamentoBatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.engjaconi.faturamentobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaturamentoBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaturamentoBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/engjaconi/faturamentobatch/FaturamentoBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.engjaconi.faturamentobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaturamentoBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
