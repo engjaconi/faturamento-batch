a36503674459c5aca1e6d9aa2a4c0df4c0ced721
Tiago Jaconi <engjaconi@gmail.com>
2024-09-04 18:46:59 -0300

    Inserido acesso ao banco de dados e configuração do job

---
+++ src/main/java/br/com/engjaconi/faturamentobatch/FaturamentoConfiguracaoJob.java
@@ -0,0 +1,8 @@
+package br.com.engjaconi.faturamentobatch;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FaturamentoConfiguracaoJob {
+    // TODO adicionar definição do job
+}
